373b68c15bfedcf3ec33cf5c3da231f3ce3422d4
HoustonSprouse919 <houstonsprouse@gmail.com>
2022-10-31 13:09:00 -0400

    playing around with animation a bit

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{const e=new IntersectionObserver((e=>{e.forEach((e=>{console.log(e),e.isIntersecting?e.target.classList.add("show"):e.target.classList.remove("show")}))}));document.querySelectorAll(".hidden").forEach((s=>e.observe(s)))})();
